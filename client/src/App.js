109c5400bf49ac1dee30225302b7f9c1a6e462fe
Hector Herrera <hectorsum81@hotmail.com>
2021-03-03 16:43:30 -0500

    auth and route validation added

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import { Navbar } from './components/layout/Navbar';
+import Navbar from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import Register from './components/auth/Register';
-import { Login } from './components/auth/Login';
+import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import "./App.css";
 
 //* Redux
 import {Provider} from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+  useEffect(()=>{
+    store.dispatch(loadUser())
+    console.log('loading')
+  },[]);
   return (
     <Provider store={store}>
       <Router>
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import { login } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
-export const Login = () => {
+const Login = ({login,isAuthenticated}) => {
   const [formData,setFormData] = useState({
     email: '',
     password: '',
@@ -10,8 +14,15 @@ export const Login = () => {
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
   const onSubmit = async(e) => {
     e.preventDefault();
-    console.log('Success')
+    login(email,password);
   }
+
+  //* Redirect if logged in
+  //* Login.js
+  if(isAuthenticated){
+    return <Redirect to="/dashboard"/>
+  }
+
   return (
     <>
       <h1 className="large text-primary">Sign In</h1>
@@ -38,3 +49,14 @@ export const Login = () => {
     </>
   )
 }
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+})
+
+export default connect(mapStateToProps,{ login })(Login);
