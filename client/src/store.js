c368510f4f7184d524bcb04a8348d033916c849f
Hector Herrera <hectorsum81@hotmail.com>
2021-03-05 12:56:35 -0500

    updating store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeEnhancers(applyMiddleware(...middleware))
   );
 
 export default store;
