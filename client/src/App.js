59158bb94902572e5e3609da3f57a0e937005779
Hector Herrera <hectorsum81@hotmail.com>
2021-03-05 12:40:46 -0500

    Posts component added as well as its config

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,8 @@ import AddExperience from './components/profile-form/AddExperience';
 import AddEducation from './components/profile-form/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -47,6 +49,8 @@ function App() {
             <PrivateRoute exact path="/edit-profile" component={EditProfile} />
             <PrivateRoute exact path="/add-experience" component={AddExperience} />
             <PrivateRoute exact path="/add-education" component={AddEducation} />
+            <PrivateRoute exact path="/posts" component={Posts} />
+            <PrivateRoute exact path="/posts/:id" component={Post} />
           </Switch>
         </section>
       </Router>
