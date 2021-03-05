6893ac5ed8886b8d3ceda964091d8371f231cfe1
Hector Herrera <hectorsum81@hotmail.com>
2021-03-05 15:06:06 -0500

    Handling 404 not found

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,8 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
+import NotFound from './components/layout/NotFound';
+import Routes from './components/routing/Routes';
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -30,29 +32,15 @@ if(localStorage.token){
 function App() {
   useEffect(()=>{
     store.dispatch(loadUser())
-    console.log('loading')
   },[]);
   return (
     <Provider store={store}>
       <Router>
         <Navbar/>
-        <Route exact path="/" component={Landing}/>
-        <section className="container">
-          <Alert/>
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/profiles" component={Profiles} />
-            <Route exact path="/profile/:id" component={Profile} />
-            <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-            <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-            <PrivateRoute exact path="/add-experience" component={AddExperience} />
-            <PrivateRoute exact path="/add-education" component={AddEducation} />
-            <PrivateRoute exact path="/posts" component={Posts} />
-            <PrivateRoute exact path="/posts/:id" component={Post} />
-          </Switch>
-        </section>
+        <Switch>
+          <Route exact path="/" component={Landing}/>
+          <Route component={Routes}/>
+        </Switch>
       </Router>
     </Provider>
   );
